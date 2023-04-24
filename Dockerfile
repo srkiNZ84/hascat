30267d0f9911b89eaad16fc364bbdf3996a1fb65
Srđan Đukić <srd@mega.co.nz>
2023-04-25 09:27:38 +1200

    Add model and simple classification code
    Add in the model so that we can add it to the Docker image (could have
    probably kept it out of git actually) as well as the ImageNet
    classifications in JSON format.
    
    Add in a sample picture.
    
    Add logic to the Flask app to return the predictions rather than a
    string.
    
    Issue(s): None

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,16 @@ COPY requirements.txt ./
 
 RUN pip install --no-cache-dir -r requirements.txt
 
+# The pretrained model (ResNet50)
+COPY resnet50_weights_tf_dim_ordering_tf_kernels.h5 /root/.keras/models/
+
+# The imagenet classifications
+COPY imagenet_class_index.json /root/.keras/models/
+
+# Our application code
 COPY app.py .
+
+# Test data
+COPY elephant.jpg .
 
 CMD ["flask", "run", "--host=0.0.0.0"]
--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+
+model = ResNet50(weights='imagenet')
+img_path = 'elephant.jpg'
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    preds = model.predict(x)
+    return "<p>Predicted: " + str(decode_predictions(preds, top=3)[0]) + "</p>"
